2634a72b29b1b829a05984d17cbba935896d945b
ttaraczuk <tiago.taraczuk96@gmail.com>
2016-02-09 11:48:33 -0200

    Initial commit

---
+++ app/src/main/java/com/creativeproject/around/fragments/MapFragment.java
@@ -0,0 +1,25 @@
+package com.creativeproject.around.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.creativeproject.around.R;
+
+public class MapFragment extends Fragment {
+
+
+    public MapFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+}
